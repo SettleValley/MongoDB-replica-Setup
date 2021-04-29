bcfbec8f8be874f1987b850bf6bfbb90c73ca016
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2021-04-28 23:39:20 -0500

    app and setup new features in db docker

---
+++ tienda/pages/_app.js
@@ -0,0 +1,10 @@
+// pages/_app.js
+import { Provider } from 'next-auth/client'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
